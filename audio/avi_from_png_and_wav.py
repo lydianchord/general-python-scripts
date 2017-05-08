89147d17d0064d00a5d51013b1363e9956065c17
lydianchord <lydianchord@users.noreply.github.com>
2017-05-08 00:24:12 -0500

    Add simple video maker

---
+++ audio/avi_from_png_and_wav.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+import sys
+
+
+def find_folder_and_image():
+    folders = [x for x in os.listdir() if '_avi' in x and os.path.isdir(x)]
+    if len(folders) == 1:
+        folder = '%s/' % folders[0]
+        print('Found folder: %s\n' % folders[0])
+    else:
+        sys.exit('Did not find folder containing "_avi" in name')
+    try:
+        image = [x for x in os.listdir(folder) if x.endswith('.png')][0]
+    except IndexError:
+        sys.exit('Did not find image')
+    return folder, image
+
+
+def make_avi(folder, png_file, wav_file):
+    subprocess.call([
+        'ffmpeg', '-y', '-i', folder + png_file, '-i', wav_file, '-c:a',
+        'pcm_s16le', '-c:v', 'libx264', '-pix_fmt', 'yuv420p', '-b:v', '9000k',
+        folder + wav_file.replace('.wav', '.avi')
+    ])
+
+
+def convert_all(folder, png_file):
+    wav_files = [x for x in os.listdir() if x.endswith('.wav')]
+    for wav_file in wav_files:
+        make_avi(folder, png_file, wav_file)
+
+
+if __name__ == '__main__':
+    convert_all(*find_folder_and_image())
